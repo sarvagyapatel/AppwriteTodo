72d97e0ae584bff27a0a9275581797e2694b7d0b
Sarvagya_Patel <patelsarvagay5@gmail.com>
2024-08-29 15:06:26 +0530

    Removed strict mode in react

--- src/main.jsx
+++ src/main.jsx
@@ -38,9 +38,7 @@ const router = createBrowserRouter([
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <Provider store={store}>
     <RouterProvider router={router}/>
-    </Provider>
-  </React.StrictMode>,
+    </Provider>,
 )
